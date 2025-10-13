6f7fe568e8fe518bf8f090e71785babb22a28263
Lev Sorokin <sorokin@fortiss.org>
2025-10-13 14:01:49 +0200

    Upload results.

---
+++ scripts/run_aggregate_over_seeds_judge.sh
@@ -0,0 +1,3 @@
+python -m scripts.aggregate_over_seeds_judge \
+        --eval_dir "C:\Users\levia\Documents\testing\LLM\opensbt-results\judge-eval\judge_eval_2025-10-13_multi\sample-3-majority" \
+        --output_csv "C:\Users\levia\Documents\testing\LLM\opensbt-results\judge-eval\judge_eval_2025-10-13_multi\sample-3-majority\combined.csv"
